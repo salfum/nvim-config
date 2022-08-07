94acec9a3bd4ea06abec88f38dae1051c06f132b
Maksim Ermakov <salfum@protonmail.com>
2022-08-08 00:04:47 +0300

    fix: reorder lsp-related plugins installation

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -10,32 +10,35 @@ enabled_lsp_filetypes = {
 }
 
 plugin({
-  'hrsh7th/nvim-cmp',
-  event = 'BufReadPre',
-  config = conf.nvim_cmp,
-  requires = {
-    { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
-    { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
-    { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
-    { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
-  },
-})
-
-plugin({
   'neovim/nvim-lspconfig',
   ft = enabled_lsp_filetypes,
   config = conf.nvim_lsp,
-  after = 'cmp-nvim-lsp'
+  requires = {
+    -- LSP support
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    -- Autocompletion
+    {
+      'hrsh7th/nvim-cmp',
+      event = 'BufReadPre',
+      config = conf.nvim_cmp,
+      after = 'nvim-lspconfig',
+    },
+    { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+    { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
+    { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
+
+    -- Snippets
+    {
+      'L3MON4D3/LuaSnip',
+      event = 'InsertEnter',
+      config = conf.lua_snip
+    },
+    { 'rafamadriz/friendly-snippets' },
+  },
 })
 
-plugin({
-  'williamboman/mason.nvim',
-})
-
-plugin({
-  'williamboman/mason-lspconfig.nvim',
-})
-
-plugin({ 'L3MON4D3/LuaSnip', event = 'InsertEnter', config = conf.lua_snip })
-
-plugin({ 'windwp/nvim-autopairs', event = 'InsertEnter', config = conf.auto_pairs })
+plugin({ 'windwp/nvim-autopairs', event = 'InsertEnter', config = conf.autopairs })
