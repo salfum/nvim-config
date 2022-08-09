fccc87e30777a795a9c894a0eb3d5f86f972beb5
Maksim Ermakov <salfum@protonmail.com>
2022-08-10 00:38:21 +0300

    feat: make packer open in floating window

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -33,8 +33,20 @@ function Packer:load_packer()
   end
   packer.init({
     compile_path = packer_compiled,
+    auto_clean = true,
+    compile_on_sync = true,
     git = { clone_timeout = 120 },
     disable_commands = true,
+    display = {
+      working_sym = "ﲊ",
+      error_sym = "✗ ",
+      done_sym = " ",
+      removed_sym = " ",
+      moved_sym = "",
+      open_fn = function()
+        return require("packer.util").float { border = "single" }
+      end,
+    },
   })
   packer.reset()
   local use = packer.use
