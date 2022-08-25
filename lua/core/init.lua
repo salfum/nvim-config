8b5b5f188bae4a99c1f3340fc8cef85648181fbb
Maksim Ermakov <salfum@protonmail.com>
2022-08-26 00:33:36 +0300

    refactor: init.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -2,7 +2,7 @@ local vim = vim
 local home = os.getenv('HOME')
 local cache_dir = home .. '/.cache/nvim/'
 
-local createdir = function()
+local create_dirs = function()
   local data_dir = {
     cache_dir .. 'backup',
     cache_dir .. 'session',
@@ -21,31 +21,38 @@ local createdir = function()
   end
 end
 
-createdir()
-
 -- disable_distribution_plugins
-vim.g.loaded_gzip = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
+local disable_distribution_plugins = function()
+  vim.g.loaded_gzip = 1
+  vim.g.loaded_tar = 1
+  vim.g.loaded_tarPlugin = 1
+  vim.g.loaded_zip = 1
+  vim.g.loaded_zipPlugin = 1
+  vim.g.loaded_getscript = 1
+  vim.g.loaded_getscriptPlugin = 1
+  vim.g.loaded_vimball = 1
+  vim.g.loaded_vimballPlugin = 1
+  vim.g.loaded_matchit = 1
+  vim.g.loaded_matchparen = 1
+  vim.g.loaded_2html_plugin = 1
+  vim.g.loaded_logiPat = 1
+  vim.g.loaded_rrhelper = 1
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+  vim.g.loaded_netrwSettings = 1
+  vim.g.loaded_netrwFileHandlers = 1
+end
 
-local pack = require('core.pack')
+local load_core = function()
+  local pack = require('core.pack')
 
-pack.ensure_plugins()
-require('core.options')
-pack.load_compile()
-require('keymap')
+  create_dirs()
+  disable_distribution_plugins()
+
+  pack.ensure_plugins()
+  require('core.options')
+  pack.load_compile()
+  require('keymap')
+end
+
+load_core()
