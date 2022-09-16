6cb75b25a95bb828a51cca11f7caccd0d3ea6e02
Maksim Ermakov <salfum@protonmail.com>
2022-09-16 22:07:47 +0300

    feat: add multiple files opening action for telescope

--- lua/modules/tools/configs/telescope.lua
+++ lua/modules/tools/configs/telescope.lua
@@ -115,88 +115,156 @@ vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
   end,
 })
 
-local plenary_status_ok, _ = pcall(require, 'plenary')
-local telescope_status_ok, _ = pcall(require, 'telescope')
+-- custom actions for multiple files opening
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local transform_mod = require('telescope.actions.mt').transform_mod
 
-if plenary_status_ok and telescope_status_ok then
-  load_highlight(group)
+local function multiopen(prompt_bufnr, method)
+  local cmd_map = {
+    vertical = 'vsplit',
+    horizontal = 'split',
+    tab = 'tabe',
+    default = 'edit',
+  }
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local multi_selection = picker:get_multi_selection()
 
-  require('telescope').setup({
-    defaults = {
-      vimgrep_arguments = {
-        'rg',
-        '--color=never',
-        '--no-heading',
-        '--with-filename',
-        '--line-number',
-        '--column',
-        '--smart-case',
-      },
-      prompt_prefix = '   ',
-      selection_caret = '  ',
-      entry_prefix = '  ',
-      initial_mode = 'insert',
-      selection_strategy = 'reset',
-      layout_strategy = 'horizontal',
-      sorting_strategy = 'ascending',
+  if #multi_selection > 1 then
+    require('telescope.pickers').on_close_prompt(prompt_bufnr)
+    pcall(vim.api.nvim_set_current_win, picker.original_win_id)
 
-      layout_config = {
-        horizontal = {
-          prompt_position = 'top',
-          preview_width = 0.55,
-          results_width = 0.8,
-        },
-        vertical = { mirror = false },
-        width = 0.87,
-        height = 0.80,
-        preview_cutoff = 120,
-      },
-      file_sorter = require('telescope.sorters').get_fuzzy_file,
-      file_ignore_patterns = file_ignore_patterns,
-      generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
-      path_display = { 'truncate' },
-      winblend = 0,
-      border = {},
-      borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-      color_devicons = true,
-      set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-
-      file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-      grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-      qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-      buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker,
-
-      mappings = {
-        n = { ['q'] = require('telescope.actions').close },
-      },
-    },
-    extensions = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = 'smart_case',
-      },
-    },
-  })
-
-  if not packer_plugins['telescope-fzf-native.nvim'].loaded then
-    vim.cmd([[packadd telescope-fzf-native.nvim]])
+    for i, entry in ipairs(multi_selection) do
+      -- opinionated use-case
+      local cmd = i == 1 and 'edit' or cmd_map[method]
+      vim.cmd(string.format('%s %s', cmd, entry.value))
+    end
+  else
+    actions['select_' .. method](prompt_bufnr)
   end
-
-  require('telescope').load_extension('fzf')
-
-  if not packer_plugins['telescope-zoxide'].loaded then
-    vim.cmd([[packadd telescope-zoxide]])
-  end
-
-  require('telescope').load_extension('zoxide')
-
-  if not packer_plugins['harpoon'].loaded then
-    vim.cmd([[packadd harpoon]])
-  end
-
-  require('telescope').load_extension('harpoon')
-
-  require('telescope').load_extension('projects')
 end
+
+local custom_actions = transform_mod({
+  multi_selection_open_vertical = function(prompt_bufnr)
+    multiopen(prompt_bufnr, 'vertical')
+  end,
+  multi_selection_open_horizontal = function(prompt_bufnr)
+    multiopen(prompt_bufnr, 'horizontal')
+  end,
+  multi_selection_open_tab = function(prompt_bufnr)
+    multiopen(prompt_bufnr, 'tab')
+  end,
+  multi_selection_open = function(prompt_bufnr)
+    multiopen(prompt_bufnr, 'default')
+  end,
+})
+
+local function stopinsert(callback)
+  return function(prompt_bufnr)
+    vim.cmd([[stopinsert]])
+    vim.schedule(function()
+      callback(prompt_bufnr)
+    end)
+  end
+end
+
+local custom_mappings = {
+  i = {
+    ['<C-v>'] = stopinsert(custom_actions.multi_selection_open_vertical),
+    ['<C-s>'] = stopinsert(custom_actions.multi_selection_open_horizontal),
+    ['<C-t>'] = stopinsert(custom_actions.multi_selection_open_tab),
+    ['<CR>'] = stopinsert(custom_actions.multi_selection_open),
+  },
+  n = {
+    ['<C-v>'] = custom_actions.multi_selection_open_vertical,
+    ['<C-s>'] = custom_actions.multi_selection_open_horizontal,
+    ['<C-t>'] = custom_actions.multi_selection_open_tab,
+    ['<CR>'] = custom_actions.multi_selection_open,
+    ['q'] = actions.close,
+  },
+}
+
+-- final assembling
+
+if not packer_plugins['plenary.nvim'].loaded then
+  vim.cmd([[packadd plenary.nvim]])
+end
+
+load_highlight(group)
+
+require('telescope').setup({
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+    },
+    prompt_prefix = '   ',
+    selection_caret = '  ',
+    entry_prefix = '  ',
+    initial_mode = 'insert',
+    selection_strategy = 'reset',
+    layout_strategy = 'horizontal',
+    sorting_strategy = 'ascending',
+
+    layout_config = {
+      horizontal = {
+        prompt_position = 'top',
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = { mirror = false },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    file_ignore_patterns = file_ignore_patterns,
+    path_display = { 'truncate' },
+    winblend = 0,
+    border = {},
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    color_devicons = true,
+    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+
+    file_sorter = require('telescope.sorters').get_fuzzy_file,
+    generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
+    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker,
+
+    mappings = custom_mappings,
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = 'smart_case',
+    },
+  },
+})
+
+if not packer_plugins['telescope-fzf-native.nvim'].loaded then
+  vim.cmd([[packadd telescope-fzf-native.nvim]])
+end
+
+require('telescope').load_extension('fzf')
+
+if not packer_plugins['telescope-zoxide'].loaded then
+  vim.cmd([[packadd telescope-zoxide]])
+end
+
+require('telescope').load_extension('zoxide')
+
+if not packer_plugins['harpoon'].loaded then
+  vim.cmd([[packadd harpoon]])
+end
+
+require('telescope').load_extension('harpoon')
+
+require('telescope').load_extension('projects')
